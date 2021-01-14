5322040f106805d45c37df1baaa0e41311389580
Tsyganov Andrey <andrytsy@mail.ru>
2021-01-14 12:09:59 +0200

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'@vue/standard',
+		'@vue/typescript/recommended'
+	],
+	parserOptions: {
+		ecmaVersion: 2020
+	},
+	rules: {
+		indent: ['error', 4, { SwitchCase: 1 }],
+		'@typescript-eslint/no-var-requires': 0,
+		'no-mixed-spaces-and-tabs': 0,
+		'no-tabs': 0
+	}
+}
