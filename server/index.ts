671b2826f3f8885f7b1aa198d015f8678dfe256c
Tsyganov Andrey <andrytsy@mail.ru>
2021-01-18 19:23:46 +0200

    Db try to connect

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// const mongodb = require('./db/index.ts')
 
 app.use(require('./router/index.ts'))
 app.use(require('./404.ts'))
+
+// mongodb.connectDB();
 
 app.listen(port, () => {
     console.log(`Backend server listening at http://localhost:${port}`)
