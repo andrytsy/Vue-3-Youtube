a9da463ab980c32f9055d2fa7f8a124535d6e2bb
Hedonism <andrytsy@mail.ru>
2021-01-19 23:54:31 +0200

    Dockerfile

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 // const mongodb = require('./db/index.ts')
+
+mongoose.connect('mongodb://mongo:27017/web-app-db', (err) => {
+	if (err) {
+		console.error('Error occurred while connecting to DB!')
+		throw err
+	} else {
+		console.log('Database connection established successfully')
+	}
+})
+
 
 app.use(require('./router/index.ts'))
 app.use(require('./404.ts'))
