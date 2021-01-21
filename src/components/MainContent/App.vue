7a0408ec884e34e6063b7ae45c525ea18be15f69
Tsyganov Andrey <andrytsy@mail.ru>
2021-01-21 16:19:50 +0200

    Refactor components

--- src/components/MainContent/App.vue
+++ src/components/MainContent/App.vue
@@ -1,18 +1,23 @@
 <template>
-    <MainContentTagsBar />
-    <MainContentGrid />
+    <template v-if="true">
+        <MainContentTagsBar />
+        <MainContentGrid />
+    </template>
+    <MainContentLoader v-else />
 </template>
 
 <script lang="ts">
     import { defineComponent } from 'vue'
     import MainContentTagsBar from '@/components/MainContent/MainContentTagsBar.vue'
     import MainContentGrid from '@/components/MainContent/MainContentGrid.vue'
+    import MainContentLoader from '@/components/MainContent/MainContentLoader.vue'
 
     export default defineComponent({
         name: 'MainContent',
         components: {
             MainContentTagsBar,
-            MainContentGrid
+            MainContentGrid,
+            MainContentLoader
         }
     })
 </script>
---
+++ src/components/MainContent/MainContentLoader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="content-loader">
+        <div class="content-loader-bar">
+            <div v-for="n in 10" :key="`content-loader-bar-item-${n}}`" class="content-loader-bar__item"></div>
+        </div>
+        <div class="content-loader-grid">
+            <div v-for="n in 10" :key="`content-loader-grid-item-${n}}`" class="content-loader-grid__item"></div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: 'MainContentItem'
+    })
+</script>
+
+<style scoped lang="scss">
+	@import "./styles/mainContentLoader.scss";
+</style>
