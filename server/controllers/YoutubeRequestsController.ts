f7c56198cc761d2ee28c2d5428a36cb397637779
Tsyganov Andrey <andrytsy@mail.ru>
2021-01-26 17:57:14 +0200

    *** RAW *** google api search

--- server/controllers/YoutubeRequestsController.ts
+++ server/controllers/YoutubeRequestsController.ts
@@ -1,22 +1,29 @@
 const {google} = require('googleapis')
+const {authenticate} = require('@google-cloud/local-auth');
+const youtube = google.youtube('v3');
+const path = require('path');
 
 declare class YoutubeRequestsController {
 	apiKey: string
-	blogger: object
 
 	constructor() {
 		this.apiKey = 'AIzaSyA9R28-FYCYK6Cg4w8wzc77iYEKsWb88ew'
-		this.blogger = google.customsearch()
 	}
 
-	getQuickOptions() {
-		this.blogger.blogs.get({}, (err, res) => {
-			if (err) {
-				console.error(err);
-				throw err;
-			}
-			console.log(`The blog url is ${res.data.url}`);
+	static async getQuickOptions(query) {
+		const auth = await authenticate({
+			keyfilePath: path.join('', './oauth2.keys.json'),
+			scopes: ['https://www.googleapis.com/auth/youtube']
 		});
+
+		google.options({auth});
+
+		const res = await youtube.search.list({
+			part: 'id,snippet',
+			q: query
+		});
+
+		console.log(res.data);
 	}
 }
 
